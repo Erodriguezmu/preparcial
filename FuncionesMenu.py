2af809ec3547e8c34c75f3b98e69d863abb86c34
Erodriguezmu <72286767+Erodriguezmu@users.noreply.github.com>
2021-12-04 22:35:40 -0500

    Update FuncionesMenu.py

--- FuncionesMenu.py
+++ FuncionesMenu.py
@@ -1,6 +1,5 @@
-''' Menu princial encargado de acceder a '''
-def menu():
-    print("Bienvenido, seleccione la opción a la que desea ingresar")
+def Menu():
+    print("Bienvenido, seleccione la operacion que desea realizar")
     print("1.) Canciones")
     print("2.) Clientes")
     print("3.) Planes")
@@ -12,14 +11,14 @@ def menu():
             opcion = int(input("Digite la opción: "))
             break
         except:
-            print("Número no válido, escoja de nuevo.")
+            print("Numero no valido, escoga denuevo.")
     return opcion
 
 def MenuOpciones():
-    print("Seleccione la operación que desea realizar")
+    print("Seleccione la operacion que desea realizar")
     print("1.) Añadir")
-    print("2.) Modificar")
-    print("3.) Eliminar")
+    print("2.) Eliminar")
+    print("3.) Modificar")
     print("4.) Consultar")
     print("5.) Salir")
 
@@ -28,39 +27,72 @@ def MenuOpciones():
             opcion = int(input("Digite la opción: "))
             break
         except:
-            print("Número no válido, escoja de nuevo.")
+            print("Numero no valido, escoga denuevo.")
     return opcion
 
 def MenuLista():
-    print("Seleccione la operación que desea realizar")
-    print("1.) Consultar")
-    print("2.) Añadir a mi lista")
-    print("3.) Eliminar de mi lista")
+    print("Seleccione la operacion que desea realizar")
+    print("1.) Añadir")
+    print("1.) Eliminar")
+    print("2.) Modificar a mi lista")
+    print("3.) consultar de mi lista")
     print("4.) Salir")
     while True:
         try:
             opcion = int(input("Digite la opción: "))
             break
         except:
-            print("Número no válido, escoja de nuevo.")
+            print("Numero no valido, escoga denuevo.")
     return opcion
     
 
-def MenuAñadir:
+#def MenuAñadir():
     
-def MenuBorrar:
+#def MenuBorrar():
+    
+#def MenuModificar():
+    
+#def MenuConsultar():
+    
 
-def MenuModificar:
-
-def MenuConsultar:
-
-def MenuCanciones:
+def MenuCanciones():
     print("1.) Nombre")
     print("2.) Genero")
     print("3.) Album")
     print("4.) Interprete")
     print("5.) Cancelar")
 
+def MenuClientes():
+    print("1.) Nombre")
+    print("2.) Apellido")
+    print("3.) Pais")
+    print("4.) Ciudad")
+    print("5.) Celular")
+    print("6.) Fecha")
+    print("7.) NTARJETA")
+    print("8.) ESTADO")
+    print("9.) Salir")
+
+def MenuPlanes():
+    print("1.) Nombre")
+    print("2.) Valor")
+    print("3.) Cantidad Canciones")
+    print("4.) Salir")
+
+def ImprimirTabla(tabla):
+    if (tabla == "CANCIONES"):
+        print("CODIGO"," ","NOMBRE"," ","GENERO"," ","ALBUM"," ","INTERPRETE")
+        print("")
+        print("")
+    if (tabla == "CLIENTES"):
+        print("CEDULA"," ","NOMBRE"," ","APELLIDO"," ","PAIS","CIUDAD","CELULAR","FECHA","NTARJETA","ESTADO")
+        print("")
+        print("")
+    if (tabla == "PLANES"):
+        print("CODIGO"," ","NOMBRE"," ","VALOR"," ","CANTIDAD")
+        print("")
+        print("")
+    
     
 
 def Salir():
@@ -71,5 +103,3 @@ def Salir():
     else:
         pass
     return condition
-                    print("Número no válido, escoja de nuevo.")
-            return opcion1
